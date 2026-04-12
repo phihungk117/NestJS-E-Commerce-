d6eb1191a38e568048ed62a09508c8a1fd0c4933
hidas2004 <phihungbp1174@gmail.com>
2026-04-13 01:33:30 +0700

    feat: update module.ts

--- src/modules/orders/orders.module.ts
+++ src/modules/orders/orders.module.ts
@@ -1,4 +1,9 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Order } from './entities/order.entity';
 
-@Module({})
+@Module({
+  // Dang ky Order entity voi TypeOrmModule
+  imports: [TypeOrmModule.forFeature([Order])],
+})
 export class OrdersModule {}
